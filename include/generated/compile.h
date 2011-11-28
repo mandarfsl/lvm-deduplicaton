eed55c1688d1bcb475e0066364607c0181052e7c
root <root@ubuntu.(none)>
2011-11-27 20:11:58 -0800

    initial code

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Sun Nov 27 18:34:15 PST 2011"
+#define LINUX_COMPILE_BY "mandar"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (Ubuntu 4.4.3-4ubuntu5) "
